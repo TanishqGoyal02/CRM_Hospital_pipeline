0ebb723168e842d2b28e0f52516f3c0d7310f012
TanishqGoyal02 <TanishqGoyal02>
2024-12-29 22:22:05 +0000

    4thinit

---
+++ cofig/adls__mount.py
@@ -0,0 +1,18 @@
+# Databricks notebook source
+storageAccountName = "adlsdevelopmentcrm"
+#storageAccountAccessKey = "INSERTstorageAccountAccessKey"
+mountPoints=["gold","silver","bronze","landing","configs"]
+for mountPoint in mountPoints:
+    if not any(mount.mountPoint == f"/mnt/{mountPoint}" for mount in dbutils.fs.mounts()):
+        try:
+            dbutils.fs.mount(
+            source = "wasbs://{}@{}.blob.core.windows.net".format(mountPoint, storageAccountName),
+            mount_point = f"/mnt/{mountPoint}",
+            extra_configs = {'fs.azure.account.key.' + storageAccountName + '.blob.core.windows.net': storageAccountAccessKey}
+            )
+            print(f"{mountPoint} mount succeeded!")
+        except Exception as e:
+            print("mount exception", e)
+
+
+dbutils.fs.mounts()
